84926077a2b393c83f4dc6f4a9be060d9f97bb04
Xenia <andreeva19-91@mail.ru>
2024-10-02 23:12:11 +0300

    add some pictures for testing

--- src/components/movie-list/movie-list.jsx
+++ src/components/movie-list/movie-list.jsx
@@ -5,20 +5,22 @@ import MovieCard from '../movie-card/movie-card';
 
 import './movie-list.css';
 import poster from './poster.jpg';
+import teacher from './teacher.jpg';
+import aviator from './aviator.jpg';
 
 class MovieList extends React.Component {
   data = [
     {
       name: 'Good movie',
       date: 'May 15, 1975',
-      poster,
+      poster: aviator,
       jenre: ['drama', 'fantasy'],
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     },
     {
       name: 'Bad movie',
       date: 'Nov 13, 1999',
-      poster,
+      poster: teacher,
       jenre: ['crime', 'horror'],
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     },
@@ -32,7 +34,7 @@ class MovieList extends React.Component {
     {
       name: 'Foo movie',
       date: 'Mar 22, 1987',
-      poster,
+      poster: aviator,
       jenre: ['bio', 'psyco'],
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     },
