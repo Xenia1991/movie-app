7862dccf0762de4a3d46a8cf2fa6cd3f6fd892ad
Xenia <andreeva19-91@mail.ru>
2024-10-10 01:32:34 +0300

    add lodash debounce

--- src/components/input-search/input-search.jsx
+++ src/components/input-search/input-search.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Input } from 'antd';
+import debounce from 'lodash.debounce';
 
 import './input-search.css';
 
 class InputSearch extends React.Component {
-  handleChange = (event) => {
+  handleChange = debounce((event) => {
     const { onChange } = this.props;
     onChange(event.target.value);
-  };
+  }, 800);
 
   render() {
     return <Input placeholder="Type to search..." size="large" onChange={this.handleChange} />;
